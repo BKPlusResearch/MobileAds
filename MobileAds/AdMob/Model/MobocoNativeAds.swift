8dd1599dbe43230ec557c24a35670e80b0297a8d
diepnn <diepnn01.it@gmail.com>
2024-08-12 17:09:47 +0700

    update MobocoAdsNative

--- MobileAds/AdMob/Model/MobocoNativeAds.swift
+++ MobileAds/AdMob/Model/MobocoNativeAds.swift
@@ -12,10 +12,12 @@ public struct MobocoNativeAds {
     public var isHighfloor: Bool = false
     public var adsNative: GADNativeAd
     public var isShowed: Bool = false
+    public var adsUnitId: String
     
-    public init(isHighfloor: Bool, adsNative: GADNativeAd, isShowed: Bool) {
+    public init(isHighfloor: Bool, adsNative: GADNativeAd, isShowed: Bool, adsUnitId: String) {
         self.isHighfloor = isHighfloor
         self.adsNative = adsNative
         self.isShowed = isShowed
+        self.adsUnitId = adsUnitId
     }
 }
