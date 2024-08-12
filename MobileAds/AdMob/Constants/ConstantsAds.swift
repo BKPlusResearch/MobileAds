66061fdbec8f4248bcf1d67670a984079234a746
diepnn <diepnn01.it@gmail.com>
2024-08-12 15:09:01 +0700

    Add class NativeAdsServices

--- MobileAds/AdMob/Constants/ConstantsAds.swift
+++ MobileAds/AdMob/Constants/ConstantsAds.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import GoogleMobileAds
 
 public typealias VoidBlockAds = () -> Void
 public typealias BoolBlockAds = (Bool) -> Void
@@ -17,6 +18,9 @@ public typealias LogBannerLoadSuccess = (_ adUnitId: String, _ precision_type: I
 public typealias LogInterstitialAdLoadSuccess = (_ adUnitId: String, _ precision_type: Int, _ revenue_micros: Int, _ ad_source_id: String, _ ad_source_name: String) -> Void
 public typealias LogRewardedAdLoadSuccess = (_ adUnitId: String, _ precision_type: Int, _ revenue_micros: Int, _ ad_source_id: String, _ ad_source_name: String) -> Void
 public typealias LogAdsOpenLoadSuccess = (_ adUnitId: String, _ precision_type: Int, _ revenue_micros: Int, _ ad_source_id: String, _ ad_source_name: String) -> Void
+
+public typealias AdsNativeLoadSuccess = (_ adUnitId: String, _ nativeAd: GADNativeAd) -> Void
+public typealias AdsNativeLoadFailure = (_ adUnitId: String, _ error: Error) -> Void
 
 let screenWidthAds = UIScreen.main.bounds.width
 let screenHeightAds = UIScreen.main.bounds.height
---
+++ MobileAds/AdMob/Service/NativeAdsServices.swift
@@ -0,0 +1,42 @@
+//
+//  NativeAdsServices.swift
+//  MobileAds
+//
+//  Created by diep on 12/8/24.
+//
+
+import Foundation
+import GoogleMobileAds
+
+open class NativeAdsServices: NSObject {
+    var onSuccessed: AdsNativeLoadSuccess?
+    var onFailed: AdsNativeLoadFailure?
+    
+    func loadAdsNative(with normalAdsID: String, rootVC: UIViewController, numberOfAds: Int, onSuccess: AdsNativeLoadSuccess?, onFail: AdsNativeLoadFailure?) {
+        self.onSuccessed = onSuccess
+        self.onFailed = onFail
+        let multipleAdsOptions = GADMultipleAdsAdLoaderOptions()
+        multipleAdsOptions.numberOfAds = numberOfAds
+        let adLoader = GADAdLoader(adUnitID: normalAdsID,
+            rootViewController: rootVC,
+            adTypes: [ .native ],
+            options: [multipleAdsOptions])
+        adLoader.delegate = self
+        adLoader.load(GADRequest())
+    }
+}
+
+extension NativeAdsServices: GADNativeAdLoaderDelegate {
+    
+    public func adLoader(_ adLoader: GADAdLoader, didReceive nativeAd: GADNativeAd) {
+        onSuccessed?(adLoader.adUnitID, nativeAd)
+    }
+    
+    public func adLoaderDidFinishLoading(_ adLoader: GADAdLoader) {
+        //
+    }
+    
+    public func adLoader(_ adLoader: GADAdLoader, didFailToReceiveAdWithError error: any Error) {
+        onFailed?(adLoader.adUnitID, error)
+    }
+}
