e86b7daee2d024f8ec95c2748dd96c291ac9cfa7
shjn <117794609+tuanvucourse@users.noreply.github.com>
2024-08-16 14:22:27 +0700

    fix crash add medium

--- MobileAds/AdMob/AdNative/Medium/MediumNativeAdView.swift
+++ MobileAds/AdMob/AdNative/Medium/MediumNativeAdView.swift
@@ -9,8 +9,9 @@ import UIKit
 import GoogleMobileAds
 
 class MediumNativeAdView: GADNativeAdView, NativeAdProtocol {
-    
+
     @IBOutlet weak var lblAds: UILabel!
+    @IBOutlet weak var adsView: UIView!
     @IBOutlet weak var actionButtonHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var lblRateCount: UILabel!
     @IBOutlet weak var starRatingImageView: UIImageView!
@@ -18,30 +19,29 @@ class MediumNativeAdView: GADNativeAdView, NativeAdProtocol {
 
     let (viewBackgroundColor, titleColor, vertiserColor, contenColor, actionColor, backgroundAction) = AdMobManager.shared.adsNativeColor.colors
     var adUnitID: String?
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = viewBackgroundColor
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        lblAds.roundCorners(corners: [.topLeft, .bottomRight], radius: 6)
         actionButtonHeightConstraint.constant = AdMobManager.shared.adsNativeMediumHeightButton
     }
-    
+
     func bindingData(nativeAd: GADNativeAd) {
         hideSkeleton()
         stopSkeletonAnimation()
         (headlineView as? UILabel)?.text = nativeAd.headline
         (callToActionView as? UIButton)?.setTitle(nativeAd.callToAction, for: .normal)
         callToActionView?.isHidden = nativeAd.callToAction == nil
-        
+
         (iconView as? UIImageView)?.image = nativeAd.icon?.image
         iconView?.isHidden = nativeAd.icon == nil
-        
+
         mediaView?.isHidden = true
-        
+
         if let star = nativeAd.starRating, let image = imageOfStars(from: star) {
             starRatingImageView.image = image
             lblRateCount.text = "\(star)"
@@ -51,19 +51,20 @@ class MediumNativeAdView: GADNativeAdView, NativeAdProtocol {
             starRatingImageView.isHidden = true
             lblRateCount.isHidden = true
         }
-        
+
         (bodyView as? UILabel)?.text = nativeAd.body
         bodyView?.isHidden = nativeAd.body == nil
-        
+
         priceLabel.text = nativeAd.price
         priceLabel.isHidden = nativeAd.price == nil
 
         (advertiserView as? UILabel)?.text = nativeAd.advertiser
         advertiserView?.isHidden = nativeAd.advertiser == nil
-                
+
         (self.callToActionView as? UIButton)?.setTitleColor(actionColor, for: .normal)
         if backgroundAction.count > 1 {
             self.callToActionView?.gradient(startColor: backgroundAction.first!, endColor: backgroundAction.last!, cornerRadius: AdMobManager.shared.adsNativeCornerRadiusButton)
+            adsView.gradient(startColor: backgroundAction.first!, endColor: backgroundAction.last!, cornerRadius: 2)
         } else {
             self.callToActionView?.layer.backgroundColor = backgroundAction.first?.cgColor
             self.callToActionView?.layer.cornerRadius = AdMobManager.shared.adsNativeCornerRadiusButton
@@ -74,14 +75,13 @@ class MediumNativeAdView: GADNativeAdView, NativeAdProtocol {
         (self.headlineView as? UILabel)?.textColor = titleColor
         (priceView as? UILabel)?.textColor = contenColor
         lblAds.textColor = AdMobManager.shared.adNativeAdsLabelColor
-        lblAds.backgroundColor = AdMobManager.shared.adNativeBackgroundAdsLabelColor
         self.backgroundColor = viewBackgroundColor
         layer.borderWidth = AdMobManager.shared.adsNativeBorderWidth
         layer.borderColor = AdMobManager.shared.adsNativeBorderColor.cgColor
         layer.cornerRadius = AdMobManager.shared.adsNativeCornerRadius
         clipsToBounds = true
-        
+
         self.nativeAd = nativeAd
     }
-    
+
 }
