db1f49a1eb7e7a9d45ed7b9a1f5178673d7a1ac3
shjn <117794609+tuanvucourse@users.noreply.github.com>
2024-08-17 23:01:21 +0700

    update small native ads

--- MobileAds/AdMob/AdNative/Small/SmallNativeAdView.swift
+++ MobileAds/AdMob/AdNative/Small/SmallNativeAdView.swift
@@ -16,61 +16,14 @@ class SmallNativeAdView: GADNativeAdView, NativeAdProtocol {
     @IBOutlet weak var cstWidthBtn: NSLayoutConstraint!
     @IBOutlet weak var viewLinePrice: UIView!
     @IBOutlet weak var stackAppStore: UIStackView!
-    @IBOutlet weak var starContainerView: UIView!
-    @IBOutlet weak var loadingView: UIView!
-    @IBOutlet weak var loadingLabel: UILabel!
-    
+    @IBOutlet weak var infoImageView: UIImageView!
+
     let (viewBackgroundColor, titleColor, _, contenColor, actionColor, backgroundAction) = AdMobManager.shared.adsNativeColor.colors
     var adUnitID: String?
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = viewBackgroundColor
-        loadingLabel.text = AdMobManager.shared.loadingAdsString
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        borderView.layer.cornerRadius = 8
-        borderView.clipsToBounds = true
-    }
-    
-    func bindingData(nativeAd: GADNativeAd) {
-        self.hideSkeleton()
-        (self.headlineView as? UILabel)?.text = nativeAd.headline
-        
-        (self.callToActionView as? UIButton)?.setTitle(nativeAd.callToAction, for: .normal)
-        self.callToActionView?.isHidden = nativeAd.callToAction == nil
-        
-        (self.iconView as? UIImageView)?.image = nativeAd.icon?.image
-        self.iconView?.isHidden = nativeAd.icon == nil
-        
-        //        if nativeAd.starRating != nil, nativeAd.starRating != 0 {
-        //            (self.starRatingView as? UIImageView)?.image = self.imageOfStars(from: nativeAd.starRating)
-        //            starContainerView.isHidden = false
-        //        } else {
-        starContainerView.removeFromSuperview()
-        starRatingView?.removeFromSuperview()
-        //        }
-
-        if nativeAd.price == nil || nativeAd.price == "" {
-            self.viewLinePrice.isHidden = true
-        } else {
-            self.viewLinePrice.isHidden = false
-        }
-        
-        if nativeAd.body == nil {
-            (self.bodyView as? UILabel)?.text = nativeAd.advertiser
-        } else {
-            (self.bodyView as? UILabel)?.text = nativeAd.body
-        }
-        
-        //        if nativeAd.body == nil {
-        //            (self.advertiserView as? UILabel)?.text = nativeAd.advertiser
-        //        } else {
-        //            (self.advertiserView as? UILabel)?.text = nativeAd.body
-        //        }
-        
         if backgroundAction.count > 1 {
             self.callToActionView?.gradient(startColor: backgroundAction.first!, endColor: backgroundAction.last!, cornerRadius: AdMobManager.shared.adsNativeCornerRadiusButton)
         } else {
@@ -78,20 +31,53 @@ class SmallNativeAdView: GADNativeAdView, NativeAdProtocol {
             self.callToActionView?.layer.cornerRadius = AdMobManager.shared.adsNativeCornerRadiusButton
         }
         (self.callToActionView as? UIButton)?.setTitleColor(actionColor, for: .normal)
-        (self.bodyView as? UILabel)?.textColor = contenColor
-        (self.headlineView as? UILabel)?.textColor = titleColor
         lblAds.textColor = AdMobManager.shared.adNativeAdsLabelColor
         lblAds.backgroundColor = AdMobManager.shared.adNativeBackgroundAdsLabelColor
+
+        (self.bodyView as? UILabel)?.textColor = contenColor
         self.backgroundColor = viewBackgroundColor
         layer.borderWidth = AdMobManager.shared.adsNativeBorderWidth
         layer.borderColor = AdMobManager.shared.adsNativeBorderColor.cgColor
         layer.cornerRadius = AdMobManager.shared.adsNativeCornerRadius
         cstWidthBtn.constant = AdMobManager.shared.adsNativeSmallWidthButton
         clipsToBounds = true
-        self.nativeAd = nativeAd
+
+        borderView.showAnimatedSkeleton()
+        lblAds.isHidden = true
+        infoImageView.isHidden = true
     }
 
-    func hideLoadingView() {
-        loadingView.isHidden = true
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        borderView.layer.cornerRadius = 8
+        borderView.clipsToBounds = true
+    }
+
+    func bindingData(nativeAd: GADNativeAd) {
+        self.hideSkeleton()
+        (self.headlineView as? UILabel)?.text = nativeAd.headline
+
+        (self.callToActionView as? UIButton)?.setTitle(nativeAd.callToAction, for: .normal)
+        self.callToActionView?.isHidden = nativeAd.callToAction == nil
+
+        (self.iconView as? UIImageView)?.image = nativeAd.icon?.image
+        self.iconView?.isHidden = nativeAd.icon == nil
+
+        if nativeAd.price == nil || nativeAd.price == "" {
+            self.viewLinePrice.isHidden = true
+        } else {
+            self.viewLinePrice.isHidden = false
+        }
+
+        if nativeAd.body == nil {
+            (self.bodyView as? UILabel)?.text = nativeAd.advertiser
+        } else {
+            (self.bodyView as? UILabel)?.text = nativeAd.body
+        }
+
+        (self.headlineView as? UILabel)?.textColor = titleColor
+        lblAds.isHidden = false
+        infoImageView.isHidden = false
+        self.nativeAd = nativeAd
     }
 }
